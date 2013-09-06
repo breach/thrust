4b06fd7c44460cf58482fddf0fce5f5e46f44632
Stanislas Polu <polu.stanislas@gmail.com>
2013-09-06 08:27:21 +0200

    Show Stack by default (for now)

--- app/lib/session.js
+++ app/lib/session.js
@@ -104,7 +104,7 @@ var session = function(spec, my) {
         my.box.init(cb_);
       },
     }, function(err) {
-      my.stack.toggle(false);
+      my.stack.toggle(true);
       my.box.show();
 
       my.exo_browser.focus(function() {
