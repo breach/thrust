2b1aacad0b52faed51c43af8e486d3238c6acc2c
Stanislas Polu <polu.stanislas@gmail.com>
2014-04-29 10:44:41 -0700

    Versions update

--- shell/index.js
+++ shell/index.js
@@ -1,13 +1,14 @@
 /*
- * ExoBrowser: index.js
+ * ExoBrowser: [shell] index.js
  *
- * (c) Copyright Stanislas Polu 2013. All rights reserved.
+ * (c) Copyright Stanislas Polu 2014. All rights reserved.
  *
  * @author: spolu
  *
  * @log:
- * 2013-08-11 spolu   Creation
+ * 2014-04-29 spolu   Move to express 4.0
  * 2013-09-06 spolu   Exp1 process.exit on session kill
+ * 2013-08-11 spolu   Creation
  */
 var express = require('express');
 var http = require('http');
@@ -25,14 +26,10 @@ var sessions = {};
 factory.log().out('Starting...');
 (function() {
   /* App Configuration */
-  app.configure(function() {
-    app.use('/', express.static(__dirname + '/controls'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-  });
+  app.use('/', express.static(__dirname + '/controls'));
+  app.use(require('body-parser')());
+  app.use(require('method-override')())
 
-  /* TODO(spolu): use the next free port */
   var http_srv = http.createServer(app).listen(8383, '127.0.0.1');
   console.error('HTTP Server started on `http://127.0.0.1:8383`');
 
@@ -62,8 +59,6 @@ factory.log().out('Starting...');
   s.on('kill', function() {
     delete sessions[s.name()];
     if(global.gc) global.gc();
-    /* TODO(spolu): For now as we have only one session, let's kill the */
-    /* process once we get here.                                        */
     process.exit(0);
   });
 })();
