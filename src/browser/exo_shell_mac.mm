829dcbc582b0447750b16a127b39d319b0ca676d
Near <nearsy.h@gmail.com>
2014-08-11 11:14:36 -0400

    fix mac coordinates

--- src/browser/exo_shell_mac.mm
+++ src/browser/exo_shell_mac.mm
@@ -211,14 +211,15 @@ ExoShell::PlatformPosition()
 
 void
 ExoShell::PlatformMove(int x, int y) {
-  [window_ setFrameTopLeftPoint:NSMakePoint(x, y)];
+  NSScreen* screen = [[NSScreen screens] objectAtIndex:0];
+  [window_ setFrameTopLeftPoint:NSMakePoint(x, NSHeight([screen frame]) - y)];
 }
 
 void
 ExoShell::PlatformResize(int width, int height) {
   NSRect frame = [window_ frame];
-  frame.origin.y -= frame.size.height; // remove the old height
-  frame.origin.y += height; // add the new height
+  frame.origin.y += frame.size.height; // remove the old height
+  frame.origin.y -= height; // add the new height
   frame.size.width = width;
   frame.size.height = height;
   [window_ setFrame:frame
