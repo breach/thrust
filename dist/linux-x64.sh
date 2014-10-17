8af9e6d251d33fa51f771045e1fecf682a961a70
Stanislas Polu <polu.stanislas@gmail.com>
2014-10-16 17:07:18 -0700

    zip instaed of tar.gz

--- dist/linux-x64.sh
+++ dist/linux-x64.sh
@@ -3,14 +3,14 @@
 cd .. && ninja -C out/Release thrust_shell -t clean && cd dist/
 cd .. && ninja -C out/Release thrust_shell -j 4 && cd dist/
 mkdir -p out && cd out
-rm -rf thrust-v0.7.0-linux-x64
+rm -rf thrust-v0.7.0-linux-x64*
 mkdir -p thrust-v0.7.0-linux-x64 && cd thrust-v0.7.0-linux-x64
 cp ../../../out/Release/thrust_shell .
 cp ../../../out/Release/*.pak .
 cp ../../../out/Release/*.so .
 cp ../../../out/Release/*.dat .
 cd ..
-tar -pczf thrust-v0.7.0-linux-x64.tar.gz thrust-v0.7.0-linux-x64
+zip -r thrust-v0.7.0-linux-x64.zip thrust-v0.7.0-linux-x64
 cd ..
 
 
