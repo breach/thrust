97e68d3b6fed3579771b58c3f342fa7d27dfe740
Stanislas Polu <stan@teleportd.com>
2014-10-16 19:35:30 -0700

    Darwin script

---
+++ dist/darwin-x64.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#cd .. && ninja -C out/Release thrust_shell -t clean && cd dist/
+#cd .. && ninja -C out/Release thrust_shell -j 4 && cd dist/
+mkdir -p out && cd out
+rm -rf thrust-v0.7.0-darwin-x64*
+mkdir -p thrust-v0.7.0-darwin-x64 && cd thrust-v0.7.0-darwin-x64
+cp -R ../../../out/Release/ThrustShell.app .
+zip -r ../thrust-v0.7.0-darwin-x64.zip *
+cd .. && cd ..
+
+
